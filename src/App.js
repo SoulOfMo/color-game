09bff3d397a5b5292ac9830876dc4ad9faf538e5
Morin Sultan <sultanmorin2001@gmail.com>
2025-02-05 09:34:08 +0100

    v1.3

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [colorOptions, setColorOptions] = useState([]);
   const [message, setMessages] = useState("");
   const [status, setStatus] = useState(false);
+  const [darkTheme, setDarkTheme] = useState("dark");
 
   function RandomColorGenerator() {
     let color = "#";
@@ -32,13 +33,14 @@ function App() {
   function Timeout() {
     setTimeout(() => {
       setStatus(false);
-    }, 2000);
+    }, 1000);
   }
 
   function handleGuess(selectedcolor) {
     if (selectedcolor === targetColor) {
       setScore((score) => score + 1);
       setMessages("Correct Guess 🎉");
+
       Timeout();
       handleReset();
     } else {
@@ -55,34 +57,65 @@ function App() {
     setColorOptions(RandomColorOptions(newTarget));
   }
 
-  function hanleNewewGame() {
+  function hanleNewGame() {
     setScore(0);
     handleReset();
     setMessages("");
   }
+
+  function handleTheme() {
+    setDarkTheme((curTheme) => (curTheme === "dark" ? "" : "dark"));
+  }
+
   useEffect(() => {
     handleReset();
   }, []);
 
   return (
-    <main>
-      <GameInstruction />
+    <main className={darkTheme === "dark" ? "bg-dark" : "bg-light"}>
+      {/* <GameInstruction handleTheme={handleTheme} themeState={darkTheme} /> */}
+      <Header handleTheme={handleTheme} themeState={darkTheme} />
       <TargetColorBox targetColor={targetColor} />
-      {/* {status && <GameStatus score={score} message={message} />} */}
-      <GameStatus score={score} message={message} />
+      <GameStatus message={message} />
       <ColorOptions
         status={status}
         options={colorOptions}
         handleGuess={handleGuess}
       />
-      <Score />
-      <ResetButton onNewGame={hanleNewewGame} />
+      <Score score={score} />
+      <ResetButton onNewGame={hanleNewGame} />
     </main>
   );
 }
 
 export default App;
 
+function Header({ themeState, handleTheme }) {
+  return (
+    <div className="header">
+      <GameInstruction />
+      <div className="toggle" onClick={handleTheme}>
+        {themeState === "dark" ? (
+          <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26">
+            <path
+              fill="#F8F9FA"
+              fill-rule="evenodd"
+              d="M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z"
+            />
+          </svg>
+        ) : (
+          <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26">
+            <path
+              fill="#2D2D2D"
+              fill-rule="evenodd"
+              d="M13 21a1 1 0 011 1v3a1 1 0 11-2 0v-3a1 1 0 011-1zm-5.657-2.343a1 1 0 010 1.414l-2.121 2.121a1 1 0 01-1.414-1.414l2.12-2.121a1 1 0 011.415 0zm12.728 0l2.121 2.121a1 1 0 01-1.414 1.414l-2.121-2.12a1 1 0 011.414-1.415zM13 8a5 5 0 110 10 5 5 0 010-10zm12 4a1 1 0 110 2h-3a1 1 0 110-2h3zM4 12a1 1 0 110 2H1a1 1 0 110-2h3zm18.192-8.192a1 1 0 010 1.414l-2.12 2.121a1 1 0 01-1.415-1.414l2.121-2.121a1 1 0 011.414 0zm-16.97 0l2.121 2.12A1 1 0 015.93 7.344L3.808 5.222a1 1 0 011.414-1.414zM13 0a1 1 0 011 1v3a1 1 0 11-2 0V1a1 1 0 011-1z"
+            />
+          </svg>
+        )}
+      </div>
+    </div>
+  );
+}
 function GameInstruction() {
   return <h1 data-testid="gameInstructions">Guess the correct color</h1>;
 }
@@ -97,7 +130,7 @@ function TargetColorBox({ targetColor }) {
   );
 }
 
-function GameStatus({ message, status }) {
+function GameStatus({ message }) {
   return (
     <p className="message" data-testid="gameStatus">
       {message}
